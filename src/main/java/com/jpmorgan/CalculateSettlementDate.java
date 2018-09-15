e6f8c8a74abcf58ae8a25f9b518a4182f1eac22f
Braffa <dave.brayfield@gmail.com>
2018-09-15 09:59:07 +0100

    added @FunctionalInterface annotation

--- src/main/java/com/jpmorgan/CalculateSettlementDate.java
+++ src/main/java/com/jpmorgan/CalculateSettlementDate.java
@@ -3,6 +3,7 @@ package com.jpmorgan;
 import java.util.Calendar;
 import java.util.Date;
 
+@FunctionalInterface
 public interface CalculateSettlementDate {
 
 	Date setSettlementDate(Calendar calendar, String currency);
--- src/main/java/com/jpmorgan/CalendarFromString.java
+++ src/main/java/com/jpmorgan/CalendarFromString.java
@@ -3,6 +3,7 @@ package com.jpmorgan;
 import java.text.ParseException;
 import java.util.Calendar;
 
+@FunctionalInterface
 public interface CalendarFromString {
 	
 	Calendar getCalendarFromString(String Date)  throws ParseException ;
